94cfe176ed45b8b42361404989f41e4b50642941
alirazagpt4 <ali.4aug24webgpt@gmail.com>
2026-09-23 12:27:47 +0500

    changes in backend

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -1,6 +1,11 @@
 using Microsoft.AspNetCore.Mvc;
 using greenslip_backend.Data;
 using greenslip_backend.DTOs;
+using BCrypt.Net;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 
 namespace greenslip_backend.Controllers;
@@ -10,10 +15,12 @@ namespace greenslip_backend.Controllers;
 public class AdminController : ControllerBase
 {
     private readonly AppDbContext _context;
+    private readonly IConfiguration _config;
 
-    public AdminController(AppDbContext context)
+    public AdminController(AppDbContext context , IConfiguration config)
     {
            _context = context;
+           _config = config;
     }
 
     // login admin request
@@ -27,8 +34,36 @@ public class AdminController : ControllerBase
             return Unauthorized(new {success = false , error = "Invalid Credentials."});
         }
 
-        
+        // check for password that comes from req body
+         bool IsPasswordValid = BCrypt.Net.BCrypt.Verify(dto.Password , admin.PasswordHash);
 
-        return Ok(new {success = true , message = "Admin mil gaya laikin abhi password check nahi hua "});
+         if(!IsPasswordValid){
+            return Unauthorized(new {success = false , error = "Invalid Password."});
+         }
+
+
+        // add claims
+        var claims = new List<Claim>
+        {
+            new Claim("AdminId" , admin.Id.ToString()),
+            new Claim("IsSuperAdmin" , admin.IsSuperAdmin.ToString()),
+            new Claim("CompanyId" , admin.CompanyId?.ToString() ?? "")
+        };
+
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+        // token creation
+        var token = new JwtSecurityToken(
+            issuer : _config["Jwt:Issuer"],
+            claims : claims,
+            expires : DateTime.Now.AddHours(24),
+            signingCredentials : creds
+        );
+
+        string tokenString = new JwtSecurityTokenHandler().WriteToken(token);
+
+        
+        return Ok(new {success = true , token = tokenString});
     }
 }
--- obj/Debug/net10.0/greenslip-backend.AssemblyInfo.cs
+++ obj/Debug/net10.0/greenslip-backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("greenslip-backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+115b4986dc0dd7049d16c15a9bb15f2402878f73")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+246941f8dec85ad5c454663d1cbd20f52630d9a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("greenslip-backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("greenslip-backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
